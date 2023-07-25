01448dc71968fcadfa3b7b4d4ec90e942c1f0064
Ivan Filip Terzic <i.terzic@gmx.net>
2023-07-25 23:50:27 +0200

    add some more plugins

--- lua/i-terzic/packer.lua
+++ lua/i-terzic/packer.lua
@@ -39,10 +39,11 @@ return require('packer').startup(function(use)
         {'hrsh7th/nvim-cmp'},     -- Required
         {'hrsh7th/cmp-nvim-lsp'}, -- Required
         {'L3MON4D3/LuaSnip'},     -- Required
-        use {
-            'nvim-lualine/lualine.nvim',
-            requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-        }
-    }
+    },
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    },
+    use { 'numToStr/comment.nvim' },
 }
 end)
